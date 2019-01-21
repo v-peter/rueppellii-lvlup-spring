9010f34dfe869bca6d43e0acfd03026ed3364390
BPAndrea <bonnyaipap.andrea@gmail.com>
2019-01-21 14:22:25 +0100

    created project

---
+++ src/main/java/com/greenfox/lvlup/LvlupApplication.java
@@ -0,0 +1,14 @@
+package com.greenfox.lvlup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvlupApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LvlupApplication.class, args);
+  }
+
+}
+
