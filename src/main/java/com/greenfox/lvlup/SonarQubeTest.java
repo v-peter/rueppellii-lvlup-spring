f4b467f664cf009e78b7d1a532754ea712c97381
Peter Vadasz <vadasz.peter@freemail.hu>
2019-01-22 16:15:55 +0100

    sonarlint

---
+++ src/main/java/com/greenfox/lvlup/SonarQubeTest.java
@@ -0,0 +1,17 @@
+package com.greenfox.lvlup;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SonarQubeTest {
+  public String method(){
+    String s;
+    if (1 == 1){
+      s = "1";
+    }
+    else {
+      s = "1";
+    }
+    return s;
+  }
+}
